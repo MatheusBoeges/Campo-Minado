2227cdd217498820a3c9a0efe83d4704f41b6438
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-09-01 19:41:01 -0300

    C5 - Criando o metodo para Adcionar Vizinhos no campo.

--- campo-minado/src/br/com/udemy/cm/modelo/Campo.java
+++ campo-minado/src/br/com/udemy/cm/modelo/Campo.java
@@ -5,18 +5,39 @@ import java.util.List;
 
 public class Campo {
 	
-	private final int Linha;
+	private final int linha;
 	private final int coluna;
 	
 	private boolean aberto = false;
 	private boolean minado = false;
 	private boolean marcado = false;
-	
+
 	private List<Campo> vizinhos = new ArrayList<>();
 	
 	Campo(int linha, int coluna) {
-		this.Linha = linha;
+		this.linha = linha;
 		this.coluna = coluna;
+	}
+	
+	boolean adcicionarVizinho(Campo candidatoVizinho) {
+		boolean linhaDiferente = linha != candidatoVizinho.linha;
+		boolean colunaDiferente = coluna != candidatoVizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+		
+		int deltaLinha = Math.abs(linha - candidatoVizinho.linha);
+		int deltaColuna = Math.abs(coluna - candidatoVizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+		
+		if(deltaGeral == 1 && !diagonal) {
+			vizinhos.add(candidatoVizinho);
+			return true;
+		} else if(deltaGeral == 2 && diagonal) {
+			vizinhos.add(candidatoVizinho);
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 
 }
