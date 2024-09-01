abfc12a1bfc0c3961e80041fe9aabe7075e67de0
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-09-01 18:39:32 -0300

    C2 - Criando os atributos na classe Campo.

--- campo-minado/src/br/com/udemy/cm/modelo/Campo.java
+++ campo-minado/src/br/com/udemy/cm/modelo/Campo.java
@@ -1,5 +1,22 @@
 package br.com.udemy.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
+	
+	private final int Linha;
+	private final int coluna;
+	
+	private boolean aberto = false;
+	private boolean minado = false;
+	private boolean marcado = false;
+	
+	private List<Campo> vizinhos = new ArrayList<>();
+	
+	Campo(int linha, int coluna) {
+		this.Linha = linha;
+		this.coluna = coluna;
+	}
 
 }
