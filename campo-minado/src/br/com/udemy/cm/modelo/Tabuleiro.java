fbbeb2dd9123e09bd16268272f41052703f3598a
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-09-10 22:17:02 -0300

    C1 - Criando os atributos na classe Tabuleiro.

--- campo-minado/src/br/com/udemy/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/udemy/cm/modelo/Tabuleiro.java
@@ -1,5 +1,24 @@
 package br.com.udemy.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
 
 }
